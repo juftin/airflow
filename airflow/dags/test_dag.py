44bdf46a9ac3203990ea352b6baae53e1eb7dd17
juftin <juftin@juftin.com>
2020-09-15 00:07:35 -0600

    Python Operator

--- airflow/dags/test_dag.py
+++ airflow/dags/test_dag.py
@@ -2,6 +2,7 @@
 Code that goes along with the Airflow tutorial located at:
 https://github.com/apache/airflow/blob/master/airflow/example_dags/tutorial.py
 """
+
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
